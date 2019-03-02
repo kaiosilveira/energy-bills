5aaf3fda0d2bc863f6b4308604082dd0c4321d2e
Kaio Silveira <silveira.kaio@outlook.com>
2019-03-02 18:19:13 -0300

    component initial structure

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const Actions = ({ firstButtonText, secondButtonText }) => (
+    <div className="actions">
+      <button className="btn-primary">
+          {firstButtonText}
+      </button>
+      <button className="btn-primary">
+          {secondButtonText}
+        </button>
+    </div>
+)
+
+const brazilianStates = [
+  { name: 'Acre', initials: 'AC' },
+  { name: 'Alagoas', initials: 'AL' },
+  { name: 'Amapá', initials: 'AP' },
+  { name: 'Amazonas', initials: 'AM' },
+  { name: 'Bahia', initials: 'BA' },
+  { name: 'Ceará', initials: 'CE' },
+  { name: 'Distrito Federal', initials: 'DF' },
+  { name: 'Espírito Santo', initials: 'ES' },
+  { name: 'Goiás', initials: 'GO' },
+  { name: 'Maranhão', initials: 'MA' },
+  { name: 'Pará', initials: 'PA' }, 
+  { name: 'Mato Grosso', initials: 'MT' },
+  { name: 'Minas Gerais', initials: 'MG' },
+  { name: 'Mato Grosso do Sul', initials: 'MS' }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Energy Bills</h1>
+
+        <Actions firstButtonText="Calcular" secondButtonText="Gráficos"/>
+
+        <div className="options">
+          <select className="select-box width-one-third">
+            <option value="">Estado</option>
+              {brazilianStates.map(state => (
+                <option key={state.initials} value={state.initials}>{state.name}</option>
+              ))}
+          </select>
+          <select className="select-box width-one-third">
+            <option value="">Mês</option>
+            <option value="1">Janeiro</option>
+            <option value="2">Fevereiro</option>
+            <option value="3">Março</option>
+          </select>
+          <div className="input-wrapper width-one-third">
+            <input className="input" type="number" placeholder="KWH"/>
+          </div>
+        </div>
+
+        <div className="calculate-btn-wrapper">
+          <button className="calculate-btn">Calcular</button>
+        </div>
+
+        <div className="card result">
+          <span>Valor aproximado</span>
+          <span className="amount">R$300,00</span>
+        </div>
+
       </div>
     );
   }
